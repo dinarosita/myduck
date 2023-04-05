1f6d2ac5311d95cbe8cd66b5cf301e610daa77a1
Dina Rosita <dina@koudys.com>
2023-04-05 17:51:03 -0400

    Hamburger icon and ul nested correctly inside a nav

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -8,16 +8,15 @@ function Header() {
   const [chatNav, setChatNav] = useState(false);
   const [pageNav, setPageNav] = useState(false);
 
-
   return (
     <div>
       <header className="to-none absolute z-20 flex h-12 w-full flex-row items-center justify-between bg-gradient-to-b from-sol-50 px-4">
         <div left className="flex flex-row items-center gap-2">
-          <div id="chatNavButton"
+          <div
+            id="chatNavButton"
             onClick={() => {
               setChatNav(!chatNav);
               setPageNav(false);
-   
             }}
             className="title cursor-pointer text-3xl"
           >
@@ -37,18 +36,43 @@ function Header() {
             </Link>
           </div>
         </div>
-        <div
-          right
-          onClick={() => {
-            setPageNav(!pageNav);
-            setChatNav(false);
-          }}
-          className="title cursor-pointer text-3xl "
-        >
-          <ion-icon
-            name={pageNav ? "close-outline" : "menu-outline"}
-          ></ion-icon>
-        </div>
+        <nav id="pageNavigation">
+          <div id="hamburgerIcon"
+            onClick={() => {
+              setPageNav(!pageNav);
+              setChatNav(false);
+            }}
+            className="title cursor-pointer text-3xl sm:hidden"
+          >
+            <ion-icon
+              name={pageNav ? "close-outline" : "menu-outline"}
+            ></ion-icon>
+          </div>
+
+          <ul id="pageOptions"
+            className={`
+              absolute top-0 z-[-10] h-screen w-full bg-white bg-opacity-90 transition-all duration-300   ${
+                pageNav ? "right-0 opacity-100" : "-right-full opacity-0"
+              }
+              
+              scrollbar flex h-full flex-col gap-2 overflow-auto pt-12 pb-4 `}
+            onClick={() => setPageNav(false)}
+          >
+            {PAGES.map((page) => (
+              <li className=" to-none bg-gradient-to-l from-sol-50">
+                <Link
+                  key={page.name}
+                  to={page.path}
+                  onClick={() => setPageNav(false)}
+                  exact
+                  className="title block h-full w-full py-2 px-6 text-right "
+                >
+                  {page.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </header>
 
       <nav
@@ -64,32 +88,6 @@ function Header() {
           <ChatHistory />
           <AddChat />
         </div>
-      </nav>
-
-      <nav
-        id="pageNav"
-        className={`absolute top-0 z-10 h-screen w-full bg-white bg-opacity-90 transition-all duration-300   ${
-          pageNav ? "right-0 opacity-100" : "-right-full opacity-0"
-        }`}
-        onClick={() => setPageNav(false)}
-      >
-        <ul
-          className={`scrollbar flex h-full flex-col gap-2 overflow-auto pt-12 pb-4 `}
-        >
-          {PAGES.map((page) => (
-            <li className=" sm:bg-none bg-gradient-to-l from-sol-50 to-none ">
-              <Link
-                key={page.name}
-                to={page.path}
-                onClick={() => setPageNav(false)}
-                exact
-                className="title block h-full w-full py-2 px-6 text-right "
-              >
-                {page.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
       </nav>
     </div>
   );
