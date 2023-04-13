aed1bfe4272c298c9aee876a2d4a1466cff71c2f
Dina Rosita <dina@koudys.com>
2023-04-12 20:14:33 -0400

    MessageHistory is chat-app like

--- src/components/contents/MessageHistory.js
+++ src/components/contents/MessageHistory.js
@@ -1,23 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import MainChatContext from "../../contexts/MainChatContext";
 
 export default function MessageHistory() {
   const { messageList, id } = useContext(MainChatContext);
+  const containerRef = useRef();
+
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight;
+    }
+  }, [messageList]);
+
+
   if (!id) {
     return <div></div>;
   }
   return (
-    <div className="flex h-full flex-auto flex-col-reverse whitespace-pre-wrap gap-2 pr-8 overflow-y-scroll rounded border-mboxborderw border-r-0 border-mboxcolor scrollbar">
+    <div ref={containerRef} className="flex flex-col h-full overflow-y-scroll flex-auto  whitespace-pre-wrap gap-1 pr-8  rounded border-mboxborderw border-r-0 border-mboxcolor scrollbar">
       {messageList
         .map((msg) => (
           <div
             key={msg.id}
-            className="w-fit rounded-r-3xl border border-l-0 border-opacity-50 border-sol pl-2 pr-4 py-2"
+            className="flex flex-col
+            
+            w-fit rounded-r-3xl border border-l-0 border-opacity-50 border-sol pl-2 pr-4 py-2 first:mt-1 last:mb-1"
+            
           >
             {msg.message}
           </div>
-        ))
-        .reverse()}
+        ))}
     </div>
   );
 }
