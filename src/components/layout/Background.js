71f976e80b61693448d6b5aaba10a6de95a7273e
Dina Rosita <dina@koudys.com>
2023-04-13 14:11:58 -0400

    Safe point

--- src/components/layout/Background.js
+++ src/components/layout/Background.js
@@ -6,7 +6,7 @@ export default function Background() {
       className={`absolute z-[-10] h-screen w-screen top-0 left-0 flex flex-col justify-between overflow-hidden bg-opacity-50`}
     >
       <div
-        className={`h-20 w-screen bg-gradient-to-b from-hovercolor`}
+        className={`h-20 w-screen bg-gradient-to-b from-sol-50`}
       ></div>
       <div
         className={`h-20 w-screen bg-gradient-to-t from-sol-50 `}
