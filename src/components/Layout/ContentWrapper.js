67b815249b0f624a3c24a1d6fb5556c5a2079a3a
Dina Rosita <dina@koudys.com>
2023-04-17 09:48:34 -0400

    Nav responsive for all sizes

--- src/components/Layout/ContentWrapper.js
+++ src/components/Layout/ContentWrapper.js
@@ -1,33 +1,37 @@
 import React, { useContext } from "react";
 import Navigation from "../Navigation/Navigation";
 import Main from "../Main/Main";
-import IonicButton from "../Common/IonicButton";
 import FlapContext from "../../contexts/FlapContext";
 
 export default function ContentWrapper() {
-  const { isFlapOpen, handleCloseFlap } = useContext(FlapContext);
+  const { isFlapOpen } = useContext(FlapContext);
 
   return (
     <div className="w-effective pass-overflow p-mainspace">
-
       {/* Overlay Nav: (-) to xs-480 */}
-      <div className={`xs:hidden`}>
-        <Navigation />
+      <div className={`pass-overflow relative xs:hidden`}>
+        <div
+          className={`w-80 max-w-full bg-white transition duration-default 
+            ${isFlapOpen ? "translate-x-0" : "-translate-x-full"}
+            absolute bottom-0 left-0 top-0 z-10`}
+        >
+          <Navigation />
+        </div>{" "}
         <Main />
       </div>
 
       {/* Sliding Nav: xs-480 to lg-1024 */}
-      <div className="pass-overflow relative hidden xs:flex lg:hidden">
+      <div className="pass-overflow relative hidden xs:block lg:hidden">
         <div
-          className={`w-52 flex-none bg-white pr-2 transition duration-300 sm:w-60  md:w-64
-    ${isFlapOpen ? "translate-x-0" : "-translate-x-full"}
-    absolute bottom-0 left-0 top-0 z-10`}
+          className={`w-52 sm:w-60 md:w-64 pr-2 bg-white transition duration-default  
+            ${isFlapOpen ? "translate-x-0" : "-translate-x-full"}
+            absolute bottom-0 left-0 top-0 z-10`}
         >
           <Navigation />
         </div>
         <div
-          className={`relative flex-1 overflow-y-auto transition-all duration-300  ${
-            isFlapOpen ? "md:ml-68 ml-56 sm:ml-64 " : "ml-0"
+          className={`pass-overflow relative transition-all duration-default  ${
+            isFlapOpen ? "ml-52 sm:ml-60 md:ml-64 " : "ml-0"
           }`}
         >
           <Main />
@@ -35,7 +39,7 @@ export default function ContentWrapper() {
       </div>
 
       {/* Permanent Nav: lg-1024 and up */}
-      <div className={`pass-overflow hidden flex-row gap-2 lg:flex`}>
+      <div className={`pass-overflow hidden lg:flex flex-row gap-2`}>
         <div className="w-72 flex-none">
           <Navigation />
         </div>
