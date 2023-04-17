ee6b0ec0d6a1e67e6ae798425fe69bdfdfdc01a8
Dina Rosita <dina@koudys.com>
2023-04-17 09:29:31 -0400

    Responsive SLider nav for medium size

--- src/components/Layout/ContentWrapper.js
+++ src/components/Layout/ContentWrapper.js
@@ -8,28 +8,33 @@ export default function ContentWrapper() {
   const { isFlapOpen, handleCloseFlap } = useContext(FlapContext);
 
   return (
-    // <div className="w-effective relative flex h-full flex-auto flex-row overflow-y-auto p-mainspace gap-mainspace">
     <div className="w-effective pass-overflow p-mainspace">
-      {/* Smartphone (-) <480px */}
+
+      {/* Overlay Nav: (-) to xs-480 */}
       <div className={`xs:hidden`}>
-        SMALL
         <Navigation />
         <Main />
       </div>
 
-      {/* Small screen (xs) >480px */}
-      <div
-        className={`pass-overflow hidden flex-row gap-2 xs:flex lg:hidden`}
-      >
-        <div className={`w-52 sm:w-60 md:w-64 flex-none ${isFlapOpen? "block" : "hidden"}`}>
+      {/* Sliding Nav: xs-480 to lg-1024 */}
+      <div className="pass-overflow relative hidden xs:flex lg:hidden">
+        <div
+          className={`w-52 flex-none bg-white pr-2 transition duration-300 sm:w-60  md:w-64
+    ${isFlapOpen ? "translate-x-0" : "-translate-x-full"}
+    absolute bottom-0 left-0 top-0 z-10`}
+        >
           <Navigation />
         </div>
-        <div className={`flex-1 overflow-y-auto `}>
+        <div
+          className={`relative flex-1 overflow-y-auto transition-all duration-300  ${
+            isFlapOpen ? "md:ml-68 ml-56 sm:ml-64 " : "ml-0"
+          }`}
+        >
           <Main />
-        </div>{" "}
+        </div>
       </div>
 
-      {/* LargeScreen (lg) >1024px */}
+      {/* Permanent Nav: lg-1024 and up */}
       <div className={`pass-overflow hidden flex-row gap-2 lg:flex`}>
         <div className="w-72 flex-none">
           <Navigation />
