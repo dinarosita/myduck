9c9061ad531ee41b974fda1500cba8be1190e0bf
Dina Rosita <dina@koudys.com>
2023-04-18 12:37:31 -0400

    Safepoint Vincent Theme. Looking nice but code needs tidy up

--- src/components/Layout/Footing.js
+++ src/components/Layout/Footing.js
@@ -3,10 +3,8 @@ import React from "react";
 export default function Footing() {
   return (
     <footer className="w-effective flex-none px-mainspace">
-      {/* <hr
-        className={` absolute left-0 h-0.5 w-screen border-0 border-x-0 border-b-0 bg-gradient-to-r from-blossom-400 via-sprig-400 to-almond-400`}
-      /> */}
-      <div className="text-sprig-50 font-bold flex-row-center justify-between  py-mainspace text-xs">
+  
+      <div className="text-petal font-bold flex-row-center justify-between px-1 pb-mainspace text-xs">
         <div>&copy; 2023 Codeyluwak </div>
         <div>Talking to my almond</div>
       </div>
