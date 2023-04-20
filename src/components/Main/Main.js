5205691f1d470f858f72cceca10160760bd73923
Dina Rosita <dina@koudys.com>
2023-04-20 15:22:43 -0400

    Safepoint: Distribute contexts deeper succesful

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,14 +2,17 @@ import React from "react";
 import ChatTitle from "./ChatTitle";
 import MessageHistory from "./MessageHistory";
 import AddMessage from "./AddMessage";
+import { ActiveChatContextProvider } from "../../contexts/ActiveChatContext";
 
 export default function Main() {
   return (
-    <main className="pass-overflow flex flex-auto flex-col justify-between white-frame">
-      <ChatTitle />
-      <MessageHistory />
-      <hr className="white-separator"/>
-      <AddMessage />
-    </main>
+    <ActiveChatContextProvider>
+      <main className="pass-overflow white-frame flex flex-auto flex-col justify-between">
+        <ChatTitle />
+        <MessageHistory />
+        <hr className="white-separator" />
+        <AddMessage />
+      </main>
+    </ActiveChatContextProvider>
   );
 }
