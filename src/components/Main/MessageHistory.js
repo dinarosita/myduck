50aba6c61b2a6db0ef80649bebc86f97596ad1bb
Dina Rosita <dina@koudys.com>
2023-04-20 01:31:29 -0400

    Safepoint: break-words fixed

--- src/components/Main/MessageHistory.js
+++ src/components/Main/MessageHistory.js
@@ -20,12 +20,12 @@ export default function MessageHistory() {
         ref={containerRef}
         className="
          bg-transparent
-        skyscroll flex flex-auto flex-col  gap-2  h-full overflow-y-scroll whitespace-pre-wrap   pr-8 min-h-80 "
+        skyscroll flex flex-auto flex-col  gap-2  h-full overflow-y-scroll whitespace-pre-wrap pr-8 min-h-80"
       >
         {messageList.map((msg) => (
           <div
             key={msg.id}
-            className="flex bg-petal/80 flex-col rounded-r-3xl  py-2 pl-2 pr-4 break-words leading-tight w-fit"
+            className="flex bg-petal/80 flex-col rounded-r-3xl  py-2 pl-2 pr-4 break-words leading-tight "
           >
             {msg.message}
           </div>
--- src/components/Navigation/AddChat.js
+++ src/components/Navigation/AddChat.js
@@ -1,17 +1,16 @@
 import React, { useContext, useRef } from "react";
 import IonicButton from "../Common/IonicButton";
 import { useChat } from "../../hooks/useChat";
-import { sanitizeInput } from '../../utils/sanitize';
+import { sanitizeInput } from "../../utils/sanitize";
 import { useWindowSize } from "../../hooks/useWindowSize";
 import FlapContext from "../../contexts/FlapContext";
 
-
 export default function AddChat() {
-    const {setIsFlapOpen} = useContext(FlapContext)
+  const { setIsFlapOpen } = useContext(FlapContext);
   const inputRef = useRef();
-  const { postNewChat } = useChat()
-  const windowSize = useWindowSize()
-  
+  const { postNewChat } = useChat();
+  const windowSize = useWindowSize();
+
   function cancelNewChat(e) {
     e.preventDefault();
     inputRef.current.value = "";
@@ -21,21 +20,21 @@ export default function AddChat() {
     e.preventDefault();
     const title = inputRef.current.value;
     const sanitizedTitle = sanitizeInput(title);
-    console.log(title, " into ", sanitizedTitle)
+    console.log(title, " into ", sanitizedTitle);
     postNewChat(sanitizedTitle).then(() => {
-        inputRef.current.value = "";
-    })
+      inputRef.current.value = "";
+    });
     if (windowSize.width < 480) {
-        setIsFlapOpen(false)
+      setIsFlapOpen(false);
     }
   }
-  
+
   return (
     <form
       onSubmit={handleSubmit}
       className="flex w-full flex-col items-center gap-1 px-3 py-2"
     >
-      <label htmlFor="title" className="text-petal text-lg font-bold">
+      <label htmlFor="title" className="text-lg font-bold text-petal">
         New Chat
       </label>
       <input
