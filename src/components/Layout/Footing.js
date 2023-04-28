2234c7891e8fc538be953dff39b52b895b2f1a77
Dina Rosita <dina@koudys.com>
2023-04-28 11:12:32 -0400

    dev version 'Dev 1.1.0'

--- src/components/Layout/Footing.js
+++ src/components/Layout/Footing.js
@@ -4,7 +4,7 @@ export default function Footing() {
   return (
     <footer className="w-effective flex-row-center h-7 flex-none justify-between px-4 text-xs font-bold text-petal">
       <div>&copy; 2023 Codeyluwak </div>
-      <div className="text-transparent/40">dev 1.1.0</div>
+      <div className="text-sprig-400">Dev 1.1.0</div>
       <div>I'm talking to my duck!</div>
     </footer>
   );
