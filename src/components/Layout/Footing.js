462886368bd04dee6d840ceac8c2b954330fc812
Dina Rosita <dina@koudys.com>
2023-04-28 11:09:59 -0400

    'Version 1.0.0' added

--- src/components/Layout/Footing.js
+++ src/components/Layout/Footing.js
@@ -4,6 +4,8 @@ export default function Footing() {
   return (
     <footer className="w-effective flex-row-center h-7 flex-none justify-between px-4 text-xs font-bold text-petal">
       <div>&copy; 2023 Codeyluwak </div>
+      <div>Version 1.1.0</div>
+
       <div>I'm talking to my duck!</div>
     </footer>
   );
