1d5a1926a856ac8e16d14d9c16a7812cd382963d
Dina Rosita <dina@koudys.com>
2023-05-02 12:30:50 -0400

    Chat Delete Progress: Positioning icons and tooltip

--- src/components/Main/ChatHeader.js
+++ src/components/Main/ChatHeader.js
@@ -37,26 +37,26 @@ export default function ChatHeader() {
         (isPageLoading || (!isNewUser && !mainChatMeta)) && "text-opacity-30"
       }`}
     >
-      <div className="relative h-6 w-full">
-        <div className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center">
+      <div className="flex flex-row justify-center items-center gap-2 items-end justify-end">
+        <div>
           <h1>{title}</h1>
         </div>
-        <div className="absolute bottom-0 right-1 top-0 flex items-center">
-          <div className="tooltip-parent">
-            <div className="tooltip-container ">
-              <IconButton
-                task="chatTitleEdit"
-                addIconClass="icon-on-white max-h-4"
-              />
-              <div className="tooltip">Edit chat title</div>
-            </div>
-            <div className="tooltip-container">
-              <IconButton
-                task="chatArchive"
-                addIconClass="icon-on-white max-h-4"
-              />
-              <div className="tooltip">Archive chat</div>
-            </div>
+        <div>
+          <div className="tooltip-container">
+            <IconButton
+              task="chatTitleEdit"
+              addButtonClass="align-bottom "
+              addIconClass="icon-chatheader  ml-auto"
+            />
+            <div className="tooltip">Edit chat title</div>
+          </div>
+          <div className="tooltip-container ">
+            <IconButton
+              task="chatArchive"
+              addButtonClass="align-bottom "
+              addIconClass="icon-chatheader ml-auto"
+            />
+            <div className="tooltip">Archive chat</div>
           </div>
         </div>
       </div>
