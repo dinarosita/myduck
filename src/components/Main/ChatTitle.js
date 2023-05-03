c70ee12bbc07df1706bb5ccf838fee9fe50587e1
Dina Rosita <dina@koudys.com>
2023-05-03 11:49:16 -0400

    Edit Chat Title: Project started. Previous edit/archive project by icons cancelled.

--- src/components/Main/ChatTitle.js
+++ src/components/Main/ChatTitle.js
@@ -3,7 +3,6 @@ import React, { useRef, useState } from "react";
 export default function ChatTitle({ title }) {
   const [editMode, setEditMode] = useState(false);
   const editTitleRef = useRef();
-  const oldTitle = title;
   return (
     <div>{editMode ? <input ref={editTitleRef} /> : <h1>{title}</h1>}</div>
   );
